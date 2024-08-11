0ed974b201bd3992ec7fa04939b3c70350820324
Noorie Garg <nooriegarg122@gmail.com>
2024-08-11 11:06:25 +0530

    first commit

---
+++ shoppinglyx/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from app import views
+
+urlpatterns = [
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('app.urls'))
+]
